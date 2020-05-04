677eb7073ca28712bc684e7efe8d43249dd03d53
Zafrul Hasan Khan (Nasim) <nasimkhan18003@gmail.com>
2020-05-05 01:33:33 +0600

    check table creation

---
+++ hibernate_class_db_git/src/hibernate_class_db_git/Hibernate_class_db_git.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hibernate_class_db_git;
+
+/**
+ *
+ * @author Zafrul Hasan Nasim
+ */
+public class Hibernate_class_db_git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
